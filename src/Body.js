f7b51e918c923c9eb8fcb459509fc8b23cb24ff1
Aleksandr <shumaherr@gmail.com>
2020-08-09 01:45:40 +0200

    Add Podcast Player

---
+++ src/Body.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PodcastPlayer from 'react-rss-podcast-player';
+
+import logo from './logo.svg';
+import './App.css';
+
+function Body() {
+    return (
+        <div className="Body">
+            <PodcastPlayer url={'https://anchor.fm/s/174c6e24/podcast/rss'} />
+        </div>
+    );
+}
+
+export default Body;
